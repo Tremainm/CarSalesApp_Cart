9f0c5e03cd6a909c0de10225ef70c8225a342531
Tremainm <147380462+Tremainm@users.noreply.github.com>
2024-12-06 15:41:33 +0000

    setup database entity class and repository

--- src/main/java/ie/atu/carsalesapp_cart/controller/CartController.java
+++ src/main/java/ie/atu/carsalesapp_cart/controller/CartController.java
@@ -1,4 +1,17 @@
 package ie.atu.carsalesapp_cart.controller;
 
+import ie.atu.carsalesapp_cart.service.CartService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
 public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService)
+    {
+        this.cartService = cartService;
+    }
 }
--- src/main/java/ie/atu/carsalesapp_cart/service/CartService.java
+++ src/main/java/ie/atu/carsalesapp_cart/service/CartService.java
@@ -1,4 +1,16 @@
 package ie.atu.carsalesapp_cart.service;
 
+import ie.atu.carsalesapp_cart.client.CartClient;
+import ie.atu.carsalesapp_cart.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CartService {
+
+    private final CartRepository cartRepository;
+
+    public CartService(CartRepository cartRepository){
+        this.cartRepository = cartRepository;
+    }
 }
